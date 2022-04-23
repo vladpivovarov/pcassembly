72d1dfce8439ba81763dea30d7aec604a6d5a4ca
Владислав <vladpivovarov2797@ya.ru>
2022-04-23 21:08:23 +0400

    modal upgrade and smooth scroll realize

---
+++ build/js/smoothscroll.js
@@ -0,0 +1,27 @@
+const navBar = document.querySelector(".header__nav");
+const links = navBar.querySelectorAll("a");
+
+
+// Реализация плавного скролла по id
+links.forEach((link) => {
+  link.addEventListener("click", (event) => {
+    event.preventDefault();
+    const linkHref = link.getAttribute("href");
+    const section = document.querySelector(linkHref);
+
+    if(section) {
+      // Скролл с помощью библиотеки (кроссбарузерно)
+      seamless.scrollIntoView(section, {
+        behavior: "smooth",
+        block: "start",
+        inline: "center",
+      });
+
+      // Обычный скролл
+      // section.scrollIntoView({
+      //   block: "start",
+      //   behavior: "smooth"
+      // })
+    }
+  })
+})
--- source/js/accordeon.js
+++ source/js/accordeon.js
@@ -11,6 +11,8 @@ contentArray.forEach((content, index) => {
   const title = content.querySelector(".program-line__title");
   const descr = content.querySelector(".program-line__descr");
 
+  descr.style.transition = "height 0.3s";
+
 
   title.addEventListener("click", () => {
 
@@ -18,13 +20,14 @@ contentArray.forEach((content, index) => {
 
       const descrAgain = contentAgain.querySelector(".program-line__descr");
       if (i === index) {
-        return;
-
+        descr.style.height = descr.scrollHeight + "px";
       } else {
-        descrAgain.classList.remove("active");
+        descrAgain.style.height = "0";
       }
     });
-
-    descr.classList.toggle("active");
   })
 })
+
+// descr.style.height = descr.scrollHeight;
+
+//   descr.style.height = "0";
